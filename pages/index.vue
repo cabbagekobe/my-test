d9d4c8b3e87d39f53501b1402b4a6ac04f2059cf
cabbagekobe <circlekdeath@gmail.com>
2021-08-20 11:10:53 +0900

    トップ

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,33 @@
 <template>
-  <NuxtLogo/>
+  <div>
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+        <h1>
+          title: {{ post.title }}
+        </h1>
+        <p>
+          id: {{ post.id }}
+        </p>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {}
+  export default {
+    data() {
+      return {
+        posts: []
+      }
+    },
+
+    async asyncData({ app }) {
+      const baseUrl = 'https://jsonplaceholder.typicode.com/posts'
+      const response = await app.$axios.$get(baseUrl)
+
+      return {
+        posts: response
+      }
+    }
+  }
 </script>
